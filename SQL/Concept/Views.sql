4e9e131d9f7a5c567964dc33e043ebf3e01ed6c0
Arka Nath <nath2107055@stud.kuet.ac.bd>
2025-03-14 23:38:38 +0600

    view added

---
+++ SQL/Concept/Views.sql
@@ -0,0 +1,30 @@
+Create database learn;
+use learn;
+
+CREATE TABLE student (
+    rollno INT PRIMARY KEY,
+    name VARCHAR(50),
+    marks INT NOT NULL,
+    grade VARCHAR(1),
+    city VARCHAR(20)
+);
+
+INSERT INTO student (rollno, name, marks, grade, city)
+VALUES
+    (101, "anil", 78, "C", "Pune"),
+    (102, "bhumika", 93, "A", "Mumbai"),
+    (103, "chetan", 85, "B", "Mumbai"),
+    (104, "dhruv", 96, "A", "Delhi"),
+    (105, "emanuel", 92, "F", "Delhi"),
+    (106, "farah", 82, "B", "Delhi");
+
+SELECT * FROM student;
+show tables;
+
+create view view1 as
+select rollno,name,marks from student;
+
+select * from view1 where marks > 90 ;
+
+drop view view1;
+
